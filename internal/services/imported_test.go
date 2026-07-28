0e0c612da7db64f95d30f2fded3550b679eb3d25
raffleberry <raffleberry42@gmail.com>
2026-07-28 12:17:22 +0530

    wip(import): favs list

--- internal/services/imported_test.go
+++ internal/services/imported_test.go
@@ -3,6 +3,7 @@ package services_test
 import (
 	"encoding/csv"
 	"os"
+	"slices"
 	"testing"
 
 	"github.com/raffleberry/riptvtime/internal/services"
@@ -23,7 +24,7 @@ func TestProcessRecsV2(t *testing.T) {
 	}
 
 	ipt := services.NewImportService(nil)
-	isrs, ieps, err := ipt.ProcessRecsV2(recsV2)
+	gotSrs, gotEps, err := ipt.ProcessRecsV2(recsV2)
 
 	if err != nil {
 		t.Error(err)
@@ -31,7 +32,7 @@ func TestProcessRecsV2(t *testing.T) {
 
 	type x struct{}
 
-	requiredSrs := map[string]struct{}{
+	wantSrs := map[string]struct{}{
 		utils.Jn("The Blacklist", 266189):             x{},
 		utils.Jn("Barry", 333072):                     x{},
 		utils.Jn("Wrecked (2016)", 310555):            x{},
@@ -40,13 +41,13 @@ func TestProcessRecsV2(t *testing.T) {
 		utils.Jn("Avatar: The Last Airbender", 74852): x{},
 	}
 
-	for _, isr := range isrs {
-		if _, ok := requiredSrs[utils.Jn(isr.Name, isr.TvTimeSId)]; !ok {
-			t.Errorf("Unexpected series: %v", isr.Name)
+	for _, sr := range gotSrs {
+		if _, ok := wantSrs[utils.Jn(sr.Name, sr.TvTimeSId)]; !ok {
+			t.Errorf("Unexpected series: %v", sr.Name)
 		}
 	}
 
-	requiredEpsCnt := map[string]int{
+	wantEpsCnt := map[string]int{
 		"You":                        30,
 		"The Blacklist":              10,
 		"Barry":                      17,
@@ -55,15 +56,52 @@ func TestProcessRecsV2(t *testing.T) {
 		"Avatar: The Last Airbender": 41,
 	}
 
-	epsCnt := map[string]int{}
+	gotEpsCnt := map[string]int{}
 
-	for _, ie := range ieps {
-		epsCnt[ie.SeriesName]++
+	for _, ie := range gotEps {
+		gotEpsCnt[ie.SeriesName]++
 	}
 
-	for k, v := range requiredEpsCnt {
-		if v != epsCnt[k] {
-			t.Errorf("Unexpected ep count for %s: %d != %d", k, v, epsCnt[k])
+	for k, v := range wantEpsCnt {
+		if v != gotEpsCnt[k] {
+			t.Errorf("Unexpected ep count for %s: %d != %d", k, v, gotEpsCnt[k])
+		}
+	}
+}
+
+func TestProcessFavsLists(t *testing.T) {
+	const filePath = "testdata/lists-prod-lists.csv"
+	csvFp, err := os.Open(filePath)
+	if err != nil {
+		t.Error(err)
+	}
+	csvRdr := csv.NewReader(csvFp)
+
+	recsV2, err := csvRdr.ReadAll()
+	if err != nil {
+		t.Error(err)
+	}
+
+	ipt := services.NewImportService(nil)
+	gotIds, err := ipt.ProcessFavs(recsV2)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	type x struct{}
+
+	wantIds := []int{75897, 79335, 82459, 95491,
+		176941, 281662, 293088, 94571, 362472, 79349,
+		244061, 74852, 425999, 332331, 366211, 452595}
+
+	if len(wantIds) != len(gotIds) {
+		t.Errorf("Unexpected series count: want(%d) != got(%d)", len(wantIds), gotIds[0])
+	}
+
+	for _, id := range gotIds {
+		if !slices.Contains(wantIds, id) {
+			t.Errorf("Unexpected series: %v", id)
 		}
 	}
 }
